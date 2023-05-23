4ad9be4e6544710396a6c8434021cf4fdff018e2
Ksu <kseniyaartemova@yandex.ru>
2023-05-23 04:22:15 +0200

    added docstrings and typehints

--- src/item.py
+++ src/item.py
@@ -6,6 +6,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
     path_to_cvs = os.path.join(os.path.dirname(__file__), "items.csv")
@@ -30,22 +31,31 @@ class Item:
 
         self.all.append(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Возвращает строку, в которой перечислены свойства класса для просмотра в режиме отладки"""
 
         return f"{self.__class__.__name__}(\'{self.name}\', {self.price}, {self.quantity})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Возвращает строку с наименованием товара, рекомендовано для просмотра пользователем"""
 
         return f"{self.name}"
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
+        """
+        Устанавливает наименование товара.
+
+        Проверки:
+
+        наименование должно быть строкой
+        длина наименования не должна превышать 10 символов
+        """
+
         if not isinstance(name, str):
             raise ValueError("Наименование должно быть строкой")
         elif len(name) > 10:
@@ -54,22 +64,38 @@ class Item:
             self.__name = name
 
     @property
-    def price(self):
+    def price(self) -> float:
         return self.__price
 
     @price.setter
-    def price(self, price):
+    def price(self, price: float | int) -> None:
+        """
+        Устанавливает стоимость товара.
+
+        Проверки:
+
+        стоимость должна быть целым числом, либо вещественным
+        """
+
         if type(price) not in (int, float):
             raise Exception("Цена должна быть числом")
         else:
             self.__price = float(price)
 
     @property
-    def quantity(self):
+    def quantity(self) -> int:
         return self.__quantity
 
     @quantity.setter
-    def quantity(self, quantity):
+    def quantity(self, quantity: int) -> None:
+        """
+        Устанавливает количество товара.
+
+        Проверки:
+
+        количество должно быть целым числом
+        """
+
         if type(quantity) is not int:
             raise Exception("Количество должно быть выражено целым числом")
         else:
@@ -81,12 +107,14 @@ class Item:
 
         :return: Общая стоимость товара.
         """
+
         return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
+
         if not isinstance(self.pay_rate, float):
             raise ValueError("Размер скидки задан неверно!")
         elif self.pay_rate > 1:
@@ -96,6 +124,12 @@ class Item:
 
     @classmethod
     def instantiate_from_csv(cls):
+        """
+        Класс-метод для создания объектов класса в соответствии с характеристиками,
+        указанными в csv-файле.
+        Путь к файлу является атрибутом класса.
+        """
+
         with open(cls.path_to_cvs) as csv_file:
             data_cvs = DictReader(csv_file)
 
@@ -107,6 +141,8 @@ class Item:
 
     @staticmethod
     def string_to_number(string):
+        """Метод для превращения строки в формат числа: целого или с плавающей точкой"""
+
         if "." in string:
             try:
                 number = float(string)
